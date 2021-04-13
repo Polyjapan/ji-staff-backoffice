957ce42f34932a4e7c8185feadfaf676dc823eaf
Louis Vialar <louis.vialar@gmail.com>
2021-04-14 01:30:40 +0200

    Add Docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
 
-  apiurl: 'https://staff.japan-impact.ch/api/v2/',
+  apiurl: 'https://staff.japan-impact.ch/api/v2',
   baseUrl: 'https://staff-admin.japan-impact.ch',
   apidomain: 'staff.japan-impact.ch',
 
   auth: {
-    apiurl: 'https://auth.japan-impact.ch/',
+    apiurl: 'https://auth.japan-impact.ch',
     clientId: 'staff-admin.japan-impact.ch'
   },
 };
